084db5cdd5884ebbdf6b527c8cb548ac47294cad
HendrickChong <114773323+HendrickChong@users.noreply.github.com>
2022-10-26 02:30:29 +0800

    Structure of Admin Main Page

---
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/AdminMain.java
@@ -0,0 +1,23 @@
+package oodj.assignment.oopskylinecarrentalsystem;
+
+public class AdminMain {
+
+    static void buttonViewCar(){
+
+    }
+    static void buttonAddCar(){
+
+    }
+    static void buttonBookHist(){
+
+    }
+
+    static void buttonGenRep(){
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
+ 
--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/Admin_Main.java
+++
@@ -1,8 +0,0 @@
-package oodj.assignment.oopskylinecarrentalsystem;
-
-public class Admin_Main {
-    public static void main(String args[]) {
-        System.out.println("IDK");
-    }
-}
- 
