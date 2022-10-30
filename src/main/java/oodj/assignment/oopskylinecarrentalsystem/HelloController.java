6c98a6a50fdc96d31d5b210f2e3478ef15b9f710
HendrickChong <114773323+HendrickChong@users.noreply.github.com>
2022-10-30 15:26:44 +0800

    New branch for add car

--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloController.java
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloController.java
@@ -3,6 +3,8 @@ package oodj.assignment.oopskylinecarrentalsystem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.IOException;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -10,5 +12,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void switch000_000() throws IOException {
+        
     }
 }
