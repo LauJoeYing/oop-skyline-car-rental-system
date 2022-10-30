5577b3b43b6a926be6f488bd711e91cbc4423582
Ang-dot <weoyyang00@gmail.com>
2022-10-30 15:07:21 +0800

    Update IC Number regex

--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/User.java
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/User.java
@@ -110,7 +110,7 @@ public class User {
             System.out.println("Please Enter Your Personal Identification Number:");
             String userPinInput = input.nextLine().trim();
 
-            String userPinRegex = "^[\\d]{6}[\\d]{2}[\\d]{4}|[\\d]{6}-[\\d]{2}-[\\d]{4}$";
+            String userPinRegex = "^[\\d]{12}|[\\d]{6}-[\\d]{2}-[\\d]{4}$";
             Pattern userPinPattern = Pattern.compile(userPinRegex);
             Matcher userPinMatcher = userPinPattern.matcher(userPinInput);
 
