0875738dd6c91d06a9059a2192e76f6f91a8feea
HendrickChong <114773323+HendrickChong@users.noreply.github.com>
2022-11-02 20:37:49 +0800

    Update HelloApplication.java

--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloApplication.java
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloApplication.java
@@ -11,10 +11,6 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();*/
         var scene = new Scene(new Pane());
         viewSwitcher.setScene(scene);
         viewSwitcher.switchTo(View.LOGIN);
