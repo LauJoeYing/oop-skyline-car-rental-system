2d1db264950562d2c3537a5e9cb4f1e89e731b7a
HendrickChong <114773323+HendrickChong@users.noreply.github.com>
2022-11-03 11:47:24 +0800

    Follow up changes

--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloController.java
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloController.java
@@ -1,10 +1,16 @@
 package oodj.assignment.oopskylinecarrentalsystem;
 
+import com.sun.javafx.stage.EmbeddedWindow;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,8 +31,10 @@ public class HelloController implements Initializable {
         menuWelcomeText.setText("Hello " +loginInputUsername);
     }
 
-    public void switchlogin_custMenu() {
-
+    public void switchlogin_custMenu() throws IOException {
+        Parent root = FXMLLoader.load(HelloController.class.getResource("FILENAME"));
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
