2f94147cd9b35d836d9606bb44995b0215534da4
HendrickChong <114773323+HendrickChong@users.noreply.github.com>
2022-12-07 05:20:33 +0800

    All UI pages

--- src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloApplication.java
+++ src/main/java/oodj/assignment/oopskylinecarrentalsystem/HelloApplication.java
@@ -12,18 +12,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-//        Parent root =  FXMLLoader.load(getClass().getResource("Login.fxml"));
-//        stage.setScene(new Scene(root, 960, 540));
-//        stage.show();
-        var scene = new Scene(new Pane());
-        ViewSwitcher.setScene(scene);
-        ViewSwitcher.switchTo(View.LOGIN);
-        stage.setScene(scene);
+        Parent root =  FXMLLoader.load(getClass().getResource("oodj/assignment/oopskylinecarrentalsystem/Login.fxml"));
+//        ViewSwitcher.setScene(scene);
+//        ViewSwitcher.switchTo(View.LOGIN);
+        stage.setScene(new Scene(root, 960, 540));
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
-        System.out.println("Welcome to OODJ Assignment!");
     }
 }
